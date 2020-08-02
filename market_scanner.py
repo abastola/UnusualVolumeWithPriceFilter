9a23dd40e7bfb65ecbd2a322152e94676d35eb31
abastola <abastola@ramapo.edu>
2020-08-02 11:58:38 -0400

    Added price filter

--- market_scanner.py
+++ market_scanner.py
@@ -19,6 +19,7 @@ import multiprocessing
 MONTH_CUTTOFF = 5
 DAY_CUTTOFF = 3
 STD_CUTTOFF = 10
+PRICE_CUTOFF = 5.00 #price filter
 
 
 class mainObj:
@@ -34,13 +35,15 @@ class mainObj:
             dateutil.relativedelta.relativedelta(months=MONTH_CUTTOFF)
         sys.stdout = open(os.devnull, "w")
         data = yf.download(ticker, pastDate, currentDate)
+        #print(data)
         sys.stdout = sys.__stdout__
-        return data[["Volume"]]
+        return data[["Volume", "Adj Close"]]
 
     def find_anomalies(self, data):
         global STD_CUTTOFF
         indexs = []
         outliers = []
+        closing_price = []
         data_std = np.std(data['Volume'])
         data_mean = np.mean(data['Volume'])
         anomaly_cut_off = data_std * STD_CUTTOFF
@@ -48,19 +51,23 @@ class mainObj:
         data.reset_index(level=0, inplace=True)
         for i in range(len(data)):
             temp = data['Volume'].iloc[i]
-            if temp > upper_limit:
+            temp_price = data['Adj Close'].iloc[i]
+
+            if temp > upper_limit and temp_price <= PRICE_CUTOFF:
                 indexs.append(str(data['Date'].iloc[i])[:-9])
                 outliers.append(temp)
-        d = {'Dates': indexs, 'Volume': outliers}
+                closing_price.append(temp_price)
+        d = {'Dates': indexs, 'Volume': outliers, 'Price': closing_price}
         return d
 
     def customPrint(self, d, tick):
-        print("\n\n\n*******  " + tick.upper() + "  *******")
-        print("Ticker is: "+tick.upper())
+        print(tick.upper() +"\n"+10*"-")
         for i in range(len(d['Dates'])):
             str1 = str(d['Dates'][i])
             str2 = str(d['Volume'][i])
-            print(str1 + " - " + str2)
+            print("Date:", str1)
+            print("Volume:", str2)
+            print("Price:", str(d['Price'][i]))
         print("*********************\n\n\n")
 
     def days_between(self, d1, d2):
@@ -85,6 +92,7 @@ class mainObj:
     def main_func(self):
         StocksController = NasdaqController(True)
         list_of_tickers = StocksController.getList()
+        #list_of_tickers = ["ATTO"]
         currentDate = datetime.datetime.strptime(
             date.today().strftime("%Y-%m-%d"), "%Y-%m-%d")
         start_time = time.time()
